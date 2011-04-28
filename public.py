38eb4790fd1c1708fe2a6ddac47919a91d85fc20
Harry Eakins <harry.eakins@googlemail.com>
2011-04-28 20:05:30 +0100

    Added a public page test case and created html/css for it

--- public.py
+++ public.py
@@ -3,14 +3,23 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 
-class PublicPage(webapp.RequestHandler):
-    def get(self):
-        template_values = {
-                "notices": ["This is a test", "Test number 2"],
+class Team:
+    def __init__(self, name, score):
+        self.name = name
+        self.match_score = score
+
+from datetime import datetime
+
+test_context = {
+        "competition_active": True,
+        "red_team": Team("ICRobot", 120),
+        "blue_team": Team("MDX", 20),
         }
 
+class PublicPage(webapp.RequestHandler):
+    def get(self):
         path = os.path.join(os.path.dirname(__file__), 'templates/public.html')
-        self.response.out.write(template.render(path, template_values))
+        self.response.out.write(template.render(path, test_context))
 
 application = webapp.WSGIApplication(
                                     [('/', PublicPage)],

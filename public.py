9794a36846d0a6f17fd36f633863646a825efcfd
Thomas Branch <tbranch88@gmail.com>
2011-05-04 19:30:00 +0100

    database done

--- public.py
+++ public.py
@@ -3,28 +3,49 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 
-test_context = {
-        "competition_active": True,
-        "red_team": {"name": "ICRobot",
-                     "match_score": {
-                            "total": 60,
-                            "normal": 40,
-                            "bonus": 20,
-                         }
-                     },
-        "blue_team": {"name": "MDX",
-                     "match_score": {
-                            "total": 20,
-                            "normal": 15,
-                            "bonus": 5,
-                         }
-                     },
-
-        }
+from databases import *
 
 class PublicPage(webapp.RequestHandler):
     def get(self):
         path = os.path.join(os.path.dirname(__file__), 'templates/public.html')
+        matches = Match.gql("ORDER BY regdate DESC").get()
+        if matches != None:
+            test_context = {
+                    "competition_active": True,
+                    "red_team": {"name": matches.red.name,
+                                 "match_score": {
+                                        "total": matches.rednormal+matches.redbonus,
+                                        "normal": matches.rednormal,
+                                        "bonus": matches.redbonus,
+                                     }
+                                 },
+                    "blue_team": {"name": matches.blue.name,
+                                 "match_score": {
+                                        "total": matches.bluenormal+matches.bluebonus,
+                                        "normal": matches.bluenormal,
+                                        "bonus": matches.bluebonus,
+                                     }
+                                 },
+            
+                    }
+        else:
+#            new1 = Team(name = "ICRobot")
+#            new1.put()
+#            new2 = Team(name = "MDX")
+#            new2.put()
+#            newm = Match()
+#            newm.red = new1
+#            newm.rednormal = 40
+#            newm.redbonus = 20
+#            newm.blue = new2
+#            newm.bluenormal = 15
+#            newm.bluebonus = 5
+#            newm.put()
+            test_context = {
+                    "competition_active": False
+                }
+
+
         self.response.out.write(template.render(path, test_context))
 
 application = webapp.WSGIApplication(
